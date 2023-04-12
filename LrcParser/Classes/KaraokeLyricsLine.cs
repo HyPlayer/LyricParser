dbb2ed25c8b16c6a2d265f40b341ba9ed162e252
Raspberry-Monster <lele2194512339@outlook.com>
2023-04-12 17:42:17 +0800

    [KaraokeParser] Update KaraokeParser Method To Prevent Incorrect Parsing Result

--- LrcParser/Classes/KaraokeLyricsLine.cs
+++ LrcParser/Classes/KaraokeLyricsLine.cs
@@ -7,19 +7,17 @@ namespace LrcParser.Classes
     {
         public IReadOnlyList<KaraokeWordInfo> WordInfos { get; }
 
-        public string CurrentLyric { get; }
+        public string CurrentLyric => string.Concat(WordInfos.Select(t => t.CurrentWords).ToArray());
 
         public int StartTime { get; }
 
         public int Duration { get; }
 
-        public KaraokeLyricsLine(IList<KaraokeWordInfo> wordInfos, int startTime, int duration)
+        public KaraokeLyricsLine(IEnumerable<KaraokeWordInfo> wordInfos, int startTime, int duration)
         {
             WordInfos = wordInfos.ToList();
             StartTime = startTime;
             Duration = duration;
-            var currentLyric = string.Concat(wordInfos.Select(t => t.CurrentWords).ToList());
-            CurrentLyric = currentLyric;
         }
     }
 }
--- LrcParser/Implementation/KaraokeParser.cs
+++ LrcParser/Implementation/KaraokeParser.cs
@@ -20,102 +20,134 @@ namespace LrcParser.Implementation
             var state = CurrentState.None;
             for (var i = 0; i < input.Length; i++)
             {
-                ref readonly var curChar = ref input[i];
-
-                if (curChar == '\n' || curChar == '\r')
+                if (i != input.Length)
                 {
-                    if (i + 1 < input.Length)
+                    ref readonly var curChar = ref input[i];
+
+                    if (curChar == '\n' || curChar == '\r' || i + 1 == input.Length)
                     {
-                        if ((input[i + 1] == '\n' || input[i + 1] == '\r')) i++;
-                    }
-                    state = CurrentState.None;
-                    karaokeWordInfos.Add(new KaraokeWordInfo(lyricStringBuilder.ToString(), wordTimespan, wordDuration));
-                    lines.Add(new KaraokeLyricsLine(karaokeWordInfos, lyricTimespan, lyricDuration));
-                    karaokeWordInfos.Clear();
-                    lyricStringBuilder.Clear();
-                    continue;
-                }
-                switch (curChar)
-                {
-                    case '[':
-                        state = CurrentState.PossiblyLyricTimestamp;
-                        continue;
-                    case ',':
-                        if (state == CurrentState.LyricTimestamp)
+                        if (i + 1 < input.Length)
                         {
-                            state = CurrentState.PossiblyLyricDuration;
-                            lyricTimespan = int.Parse(timespanStringBuilder.ToString());
-                            timespanStringBuilder.Clear();
+                            if ((input[i + 1] == '\n' || input[i + 1] == '\r')) i++;
                         }
-                        else if (state == CurrentState.WordTimestamp)
-                        {
-                            state = CurrentState.PossiblyWordDuration;
-                            wordTimespan = int.Parse(timespanStringBuilder.ToString());
-                            timespanStringBuilder.Clear();
-                        }
-                        else
-                        {
-                            state = CurrentState.WordUnknownItem;
-                            wordDuration = int.Parse(timespanStringBuilder.ToString());
-                            timespanStringBuilder.Clear();
-                        }
-                        continue;
-                    case ']':
                         state = CurrentState.None;
-                        lyricDuration = int.Parse(timespanStringBuilder.ToString());
-                        timespanStringBuilder.Clear();
+                        karaokeWordInfos.Add(new KaraokeWordInfo(lyricStringBuilder.ToString(), wordTimespan, wordDuration));
+                        lines.Add(new KaraokeLyricsLine(karaokeWordInfos, lyricTimespan, lyricDuration));
+                        karaokeWordInfos.Clear();
+                        lyricStringBuilder.Clear();
                         continue;
-                    case '(':
-                        if (state == CurrentState.Lyric)
-                        {
-                            karaokeWordInfos.Add(new KaraokeWordInfo(lyricStringBuilder.ToString(), wordTimespan, wordDuration));
-                            lyricStringBuilder.Clear();
-                        }
-                        state = CurrentState.PossiblyWordTimestamp;
-                        continue;
-                    case ')':
-                        state = CurrentState.Lyric;
-                        continue;
-                }
-                switch (state)
-                {
-                    case CurrentState.PossiblyLyricTimestamp:
-                        if (char.IsNumber(curChar)) state = CurrentState.LyricTimestamp;
-                        timespanStringBuilder.Append(curChar);
-                        break;
-                    case CurrentState.LyricTimestamp:
-                        timespanStringBuilder.Append(curChar);
-                        break;
-                    case CurrentState.PossiblyWordTimestamp:
-                        if (char.IsNumber(curChar)) state = CurrentState.WordTimestamp;
-                        timespanStringBuilder.Append(curChar);
-                        break;
-                    case CurrentState.WordTimestamp:
-                        timespanStringBuilder.Append(curChar);
-                        break;
-                    case CurrentState.PossiblyLyricDuration:
-                        if (char.IsNumber(curChar)) state = CurrentState.LyricDuration;
-                        timespanStringBuilder.Append(curChar);
-                        break;
-                    case CurrentState.LyricDuration:
-                        timespanStringBuilder.Append(curChar);
-                        break;
-                    case CurrentState.PossiblyWordDuration:
-                        if (char.IsNumber(curChar)) state = CurrentState.WordDuration;
-                        timespanStringBuilder.Append(curChar);
-                        break;
-                    case CurrentState.WordDuration:
-                        timespanStringBuilder.Append(curChar);
-                        break;
-                    case CurrentState.Lyric:
-                        lyricStringBuilder.Append(curChar);
-                        break;
+                    }
+                    switch (curChar)
+                    {
+                        case '[':
+                            if (state == CurrentState.Lyric)
+                            {
+                                if (i + 1 < input.Length)
+                                {
+                                    if (!char.IsNumber(input[i + 1])) break;
+                                }
+                            }
+                            state = CurrentState.PossiblyLyricTimestamp;
+                            continue;
+                        case ',':
+                            if (state == CurrentState.Lyric)
+                            {
+                                if (i + 1 < input.Length)
+                                {
+                                    if (!char.IsNumber(input[i + 1])) break;
+                                }
+                            }
+                            if (state == CurrentState.LyricTimestamp)
+                            {
+                                state = CurrentState.PossiblyLyricDuration;
+                                lyricTimespan = int.Parse(timespanStringBuilder.ToString());
+                                timespanStringBuilder.Clear();
+                            }
+                            else if (state == CurrentState.WordTimestamp)
+                            {
+                                state = CurrentState.PossiblyWordDuration;
+                                wordTimespan = int.Parse(timespanStringBuilder.ToString());
+                                timespanStringBuilder.Clear();
+                            }
+                            else
+                            {
+                                state = CurrentState.WordUnknownItem;
+                                wordDuration = int.Parse(timespanStringBuilder.ToString());
+                                timespanStringBuilder.Clear();
+                            }
+                            continue;
+                        case ']':
+                            if (state == CurrentState.Lyric)
+                            {
+                                if (i + 1 < input.Length)
+                                {
+                                    if (!char.IsNumber(input[i + 1])) break;
+                                }
+                            }
+                            state = CurrentState.None;
+                            lyricDuration = int.Parse(timespanStringBuilder.ToString());
+                            timespanStringBuilder.Clear();
+                            continue;
+                        case '(':
+                            if (state == CurrentState.Lyric)
+                            {
+                                if (i + 1 < input.Length)
+                                {
+                                    if (!char.IsNumber(input[i + 1])) break;
+                                }
+                                karaokeWordInfos.Add(new KaraokeWordInfo(lyricStringBuilder.ToString(), wordTimespan, wordDuration));
+                                lyricStringBuilder.Clear();
+                            }
+                            state = CurrentState.PossiblyWordTimestamp;
+                            continue;
+                        case ')':
+                            if (state == CurrentState.Lyric)
+                            {
+                                if (i + 1 < input.Length)
+                                {
+                                    if (!char.IsNumber(input[i + 1])) break;
+                                }
+                            }
+                            state = CurrentState.Lyric;
+                            continue;
+                    }
+                    switch (state)
+                    {
+                        case CurrentState.PossiblyLyricTimestamp:
+                            if (char.IsNumber(curChar)) state = CurrentState.LyricTimestamp;
+                            timespanStringBuilder.Append(curChar);
+                            break;
+                        case CurrentState.LyricTimestamp:
+                            timespanStringBuilder.Append(curChar);
+                            break;
+                        case CurrentState.PossiblyWordTimestamp:
+                            if (char.IsNumber(curChar)) state = CurrentState.WordTimestamp;
+                            timespanStringBuilder.Append(curChar);
+                            break;
+                        case CurrentState.WordTimestamp:
+                            timespanStringBuilder.Append(curChar);
+                            break;
+                        case CurrentState.PossiblyLyricDuration:
+                            if (char.IsNumber(curChar)) state = CurrentState.LyricDuration;
+                            timespanStringBuilder.Append(curChar);
+                            break;
+                        case CurrentState.LyricDuration:
+                            timespanStringBuilder.Append(curChar);
+                            break;
+                        case CurrentState.PossiblyWordDuration:
+                            if (char.IsNumber(curChar)) state = CurrentState.WordDuration;
+                            timespanStringBuilder.Append(curChar);
+                            break;
+                        case CurrentState.WordDuration:
+                            timespanStringBuilder.Append(curChar);
+                            break;
+                        case CurrentState.Lyric:
+                            lyricStringBuilder.Append(curChar);
+                            break;
 
+                    }
                 }
             }
-            karaokeWordInfos.Add(new KaraokeWordInfo(lyricStringBuilder.ToString(), wordTimespan, wordDuration));
-            lines.Add(new KaraokeLyricsLine(karaokeWordInfos, lyricTimespan, lyricDuration));
-            lyricStringBuilder.Clear();
             return lines;
         }
 
