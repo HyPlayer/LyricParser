591bc3936c24bfca3a77b7e56ba67f578467607c
Kengwang <github@kengwang.com.cn>
2024-02-21 15:09:18 +0800

    [feat] Add Transliteration to Word

--- LyricParser/Abstraction/KaraokeWordInfo.cs
+++ LyricParser/Abstraction/KaraokeWordInfo.cs
@@ -1,17 +1,22 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace LyricParser.Abstraction
 {
+    [DebuggerDisplay("Word = {CurrentWords}, Transliteration = {Transliteration}")]
     public sealed class KaraokeWordInfo
     {
         public string CurrentWords;
         public TimeSpan StartTime;
         public TimeSpan Duration;
+        public string? Transliteration;
         public KaraokeWordInfo(string currentWords, TimeSpan startTime, TimeSpan duration)
         {
             CurrentWords = currentWords;
             StartTime = startTime;
             Duration = duration;
         }
+
+        
     }
 }
